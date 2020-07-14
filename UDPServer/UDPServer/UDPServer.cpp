76ed2af4ad23eb647055b449fa6016a4405f2b77
Himuro-dotNET <v-takahi@microsoft.com>
2020-07-14 15:26:04 +0900

    training

---
+++ UDPServer/UDPServer/UDPServer.cpp
@@ -0,0 +1,238 @@
+﻿#include <locale.h>
+#include <Windows.h>
+#include "framework.h"
+#include "UDPServer.h"
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_LOADSTRING 100
+
+// グローバル変数:
+HINSTANCE hInst;                                // 現在のインターフェイス
+WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
+WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+SOCKET sock;
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: ここにコードを挿入してください。
+
+    // グローバル文字列を初期化する
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_UDPSERVER, szWindowClass, MAX_LOADSTRING);
+
+    MyRegisterClass(hInstance);
+
+    // アプリケーション初期化の実行:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_UDPSERVER));
+
+    MSG msg;
+
+    // メイン メッセージ ループ:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+
+
+//
+//  関数: MyRegisterClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_UDPSERVER));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_UDPSERVER);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的: メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND  - アプリケーション メニューの処理
+//  WM_PAINT    - メイン ウィンドウを描画する
+//  WM_DESTROY  - 中止メッセージを表示して戻る
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        WSAData wsaData;
+        sockaddr_in addr;
+
+        WSAStartup(MAKEWORD(2, 0), &wsaData);
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        WSAAsyncSelect(sock, hWnd, WM_WINSOCKEVENT, FD_READ | FD_CLOSE);
+
+        // アドレス等の情報を格納する
+        // IPv4
+        addr.sin_family = AF_INET;
+        // 通信ポート番号設定
+        addr.sin_port = htons(12345);
+        // INADDR_ANYはすべてのアドレスからのパケットを受信する
+        addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+        // バインド アドレス等の情報を socket に登録する
+        bind(sock, (sockaddr*)&addr, sizeof(addr));
+
+        break;
+    case WM_WINSOCKEVENT:
+        switch (WSAGETSELECTEVENT(lParam)) {
+        case FD_READ:
+            sockaddr_in oFromAddr;
+            int sockaddr_in_size = sizeof(sockaddr_in);
+
+            CHAR szData[2048];
+            recvfrom(sock, szData, sizeof(szData) - 1, 0, (sockaddr*)&oFromAddr, &sockaddr_in_size);
+
+            // ワイド文字列へ変換
+            WCHAR wStrW[2048];
+            size_t wLen = 0;
+            errno_t err = 0;
+            setlocale(LC_ALL, "japanese");
+            err = mbstowcs_s(&wLen, wStrW, 256, szData, _TRUNCATE);
+
+            // 受信したメッセージを出力
+            MessageBox(hWnd, wStrW, TEXT("Received Message"), NULL);
+            MessageBox(hWnd, TEXT("メッセージを送り返します。"), TEXT("Send Message"), NULL);
+
+            // アドレス等格納
+            struct sockaddr_in addr;
+            addr.sin_family = AF_INET;  //IPv4
+            addr.sin_port = htons(12345);   //通信ポート番号設定
+            addr.sin_addr.S_un.S_addr = inet_addr("172.17.222.203"); // 送信アドレスを127.0.0.1に設定
+
+            // 受信したメッセージをクライアントに送り返す処理
+            // sendto(ソケット, 送信するデータ, データのバイト数, フラグ, アドレス情報, アドレス情報のサイズ);
+            sendto(sock, szData, sizeof(szData), 0, (struct sockaddr*)&addr, sizeof(addr));
+            MessageBox(hWnd, TEXT("メッセージを送り返しました！！"), TEXT("Return Message"), NULL);
+
+            break;
+        }
+
+        break;
+
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // 選択されたメニューの解析:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: HDC を使用する描画コードをここに追加してください...
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
